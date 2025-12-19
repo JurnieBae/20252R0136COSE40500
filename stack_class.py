4194087307b3d4847db019fe3a6ee3ce178b6f20
JieonBae <bje3720@gmail.com>
2025-12-19 20:09:17 +0900

    Add simple stack class using Python list

---
+++ stack_class.py
@@ -0,0 +1,14 @@
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        return self.items.pop() if self.items else None
+
+s = Stack()
+s.push(10)
+s.push(20)
+print(s.pop())
